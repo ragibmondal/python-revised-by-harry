12987c333098f726ec560fcc94bc6d6025090999
ragibmondal <hasan2305101775@diu.edu.bd>
2025-01-28 16:59:55 +0600

    revised

---
+++ chapter-2/practices/problem1.py
@@ -0,0 +1,4 @@
+a=int(input("Enter Your 1st number:"))
+b=int(input("Enter Your nd number:"))
+c=a+b
+print("Addition of two Number is: ",c)
---
+++ chapter-2/practices/problem6.py
@@ -0,0 +1,4 @@
+# Write a python program to calculate the square of a number entered by the user.
+a=int (input("Enter Your Number:"))
+b=a**2
+print("Square of the number is ", b)
