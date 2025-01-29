612aa47f1d2b3c68059dc8d00fc764fbb237727e
ragibmondal <hasan2305101775@diu.edu.bd>
2025-01-29 17:02:05 +0600

    restored

---
+++ Chapt_2/03_rulevari.py
@@ -0,0 +1,7 @@
+a = 23
+aaa = 435
+harry = 34
+sameer = 40
+_samerr = 67
+# @rasheed = 56 # Invalid due to @ symbol
+# r@sheed # Invalid due to @ symbol
---
+++ Chapt_2/practices/problem2.py
@@ -0,0 +1,5 @@
+#Write a python program to find remainder when a number is divided by z.
+a=int(input("Enter your Number:"))
+z=int(input("Enter number to devide by: "))
+c=a%z
+print("Reminder is: ",c)
